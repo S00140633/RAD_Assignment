ae6e2dd44432fd2f51e80d09fa682860d25bea71
Francis Dillon <Francis Dillon>
2016-03-25 15:52:42 +0000

    confirm delete modal for customers and bookings

--- RAD_Assignment/ViewModels/CustomersVM.js
+++ RAD_Assignment/ViewModels/CustomersVM.js
@@ -13,10 +13,14 @@
             toastr.error("oops something went wrong")
         });
     }
+
+    //url for APIs
     var customerUri = '/api/customers/'
     var bookingsUri = '/api/bookings/'
     var uri = 'https://trailapi-trailapi.p.mashape.com/'
 
+
+    //Obersvable arrays for storing booking, customers and activities
     self.customers = ko.observableArray();
 
     self.bookings = ko.observableArray();
@@ -28,6 +32,7 @@
     self.Activities = ko.observableArray();
 
 
+    // retrieves all customers
     self.GetCustomers = function () {
         ajaxHelper(customerUri, 'GET').done(function (data) {
             self.customers(data);
@@ -35,30 +40,44 @@
         })
     }
 
+    //retrieves all bookings
     self.GetBookings = function () {
         ajaxHelper(bookingsUri, 'GET').done(function (data) {
             self.bookings(data);
         })
     }
 
+    //displays create customer form
     self.CreateCustomer = function () {
         $('.panel-body, .createbtn').hide();
         $('#CreateCustomer, #crteCust').show();
 
     }
 
+    //object to stores customer details
     self.newCustomer = {
         FirstName: ko.observable(),
         LastName: ko.observable(),
         DOB: ko.observable(),
     }
 
+    //posts data to create new customer
     self.CreateNewCustomer = function () {
         ajaxHelper(customerUri, 'POST', self.newCustomer).done(function (data) {
-            self.customers.push(data);
+            //clears form
+            self.newCustomer.FirstName('')
+            self.newCustomer.LastName('')
+            self.newCustomer.DOB('')
+
+            self.customers.push(data);// adds new customer to observable array
             $('#CreateCustomer, #crteCust').hide();
             $('.panel-body, .createbtn').show();
         })
+    }
+    //cancel create customer
+    self.CancelCreateCustomer = function () {
+        $('#CreateCustomer, #crteCust').hide();
+        $('.panel-body, .createbtn').show();
     }
     //observables to store selected customer
     self.CustomerName = ko.observable();
@@ -69,36 +88,35 @@
         self.BookingsForCustomer.removeAll()//clear observable Array
         self.CustomerName(customer.FirstName)
         self.CustomerId(customer.Id)
-
-        ko.utils.arrayForEach(self.bookings(), function (item) {
+        $('#NewBooking').hide();//add selected customer bookings
+        $('#CreateBooking, #AllBookings').show();
+        ko.utils.arrayForEach(self.bookings(), function (item) {// sorts through all bookings and picks out the selected customer bookings
             if (item.CustomerId == customer.Id) {
-                self.BookingsForCustomer.push(item);
-                $('#NewBooking').hide();//add selected customer bookings
-                $('#CreateBooking, #AllBookings').show();
-                             
+                self.BookingsForCustomer.push(item);                             
             }
         })
     }
 
+    //displays create new booking page
     self.CreateBooking = function () {
         $('#AllBookings').hide();
         $('#NewBooking').show();
-        toastr.success('Click on booking to see activities')
+        toastr.success('Please select a Country')
     }
 
 
-
+    //when user selects country it calls the Trail API and passes it the country as a parameter
     $('.dropdown-menu li').on('click', function () {
         var country = $(this).text();
         self.places.removeAll()
         $.ajax({
-            url: uri, // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+            url: uri, // The URL to the API. You can get this by clicking 
             type: 'GET', // The HTTP Method
             data: { 'q[country_cont]': country, 'limit': '20' }, // Additional parameters here
             datatype: 'json',
             success: function (data) {
                 $.each(data.places, function (i, v) {
-                    self.places.push(v)
+                    self.places.push(v)// add returned data to observable array
                     $('.list-group').show();
                     toastr.success('Select an Activity Centre to see activities')
                 })
@@ -113,12 +131,13 @@
     });
     self.ActivityId = ko.observable();
 
+    // displays activities for an selected activity centre
     self.CentreActivies = function (centre) {
         $('#Activitydetails').show();
 
         self.Activities.removeAll();
         self.ActivityId(centre.unique_id);
-        ko.utils.arrayForEach(self.places(), function (item) {
+        ko.utils.arrayForEach(self.places(), function (item) {// finds related activities to selected Activity centre
             if (item.unique_id == centre.unique_id) {
                 self.Activities.push(item);
 
@@ -128,7 +147,7 @@
     }
 
 
-
+    // object to strore new booking details
     self.NewBooking = {
         Country: ko.observable(),
         City: ko.observable(),
@@ -154,8 +173,11 @@
                 self.NewBooking.BookingDate(bookingdate);
                 self.NewBooking.CustomerId(self.CustomerId())
 
+                //posts booking info to web api
                 ajaxHelper(bookingsUri, 'POST', self.NewBooking).done(function (data) {
                     self.BookingsForCustomer.push(data);
+                    $('#datepicker1').val('')
+                    self.GetBookings();
                     toastr.success("Booking Confirmed");
                     $('#NewBooking').hide();
                     $('#AllBookings').show();
@@ -165,22 +187,49 @@
 
 
     }
-    self.DeletedId = ko.observable();
+    //store deleted booking until the deletion is confirmed
+    self.DeletedBooking = ko.observable();
 
-    self.DeleteBooking = function (booking) {
-        $.ajax({
-            url: bookingsUri + booking.Id,
-            type:'DELETE',
-            success: function (data) {
-                self.BookingsForCustomer.remove(data);
-                toastr.success('Booking Deleted')
-            },
-            error: function () {
-                toastr.error('Sorry. Booking Not Deleted')
-            }
+    //store deleted customer until the deletion is confirmed
+    self.DeletedCustomer = ko.observable();
+
+    var deleted;
+
+    // show confirm delete booking modal
+    self.showModal = function (deleting) {
+        deleted = deleting.Id;
+        self.DeletedBooking(deleting)
+        $('#delete').modal('show');
+    };
+
+    // show confirm delete customer modal
+    self.showModalDelete = function (deleting) {
+        deleted = deleting.Id;
+        self.DeletedCustomer(deleting)
+        $('#deleteCust').modal('show');
+    };
+
+    //delete booking
+    self.DeleteBooking = function () {
+        ajaxHelper(bookingsUri + deleted, 'Delete').done(function (data) {
+            self.GetBookings();
+            self.BookingsForCustomer.remove(self.DeletedBooking());//remove the deleted booking form the observable array
+            toastr.success('Booking Deleted')
+            $('#delete').modal('hide');
         })
-
     }
+
+    //delete Customer
+    self.DeleteCustomer = function () {
+        ajaxHelper(customerUri + deleted, 'Delete').done(function (data) {
+            self.customers.remove(self.DeletedCustomer());//remove the deleted booking form the observable array
+            toastr.success('Customer Deleted')
+            $('#CreateBooking, #AllBookings').hide();
+            $('#deleteCust').modal('hide');
+        })
+    }
+
+    // helps displays multiple google maps with different latitude and longitude
     ko.bindingHandlers.googlemap = {
                 init: function (element, valueAccessor) {
                     var
@@ -208,11 +257,12 @@ $(document).ready(function () {
     toastr.options.preventDuplicates = true;
     var CVM = new CustomersVM;
 
+    //initialises datepickers
     $("#datepicker").datepicker();
     $("#datepicker1").datepicker({
         onSelect: function () {
             var empty = false;
-
+            //check if date has been selected
             if ($(this).val() == '') {
                 empty = true;
             }
@@ -245,5 +295,5 @@ $(document).ready(function () {
 
     $('#BookNow').attr('disabled', 'disabled')
 
-    ko.applyBindings(CVM)
+    ko.applyBindings(CVM)// binds viewModel
 })
